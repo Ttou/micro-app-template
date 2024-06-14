2e06e9c8cf990794fcbe525bb4ea7f631c530994
wangruijin <1361572192@qq.com>
2024-06-14 19:59:39 +0800

    feat(custom): 全部启用 umd 模式

--- apps/app-rs-vue3/src/main.ts
+++ apps/app-rs-vue3/src/main.ts
@@ -1,20 +1,45 @@
 import './assets/styles'
 
-import { createApp } from 'vue'
+import { type App as IApp, createApp } from 'vue'
+import {
+  createRouter,
+  createWebHashHistory,
+  Router,
+  RouterHistory
+} from 'vue-router'
 
 import App from './App.vue'
 import { useElementPlus, useErrorHandler } from './hooks'
-import router from './router'
+import { routes } from './router'
 
-const app = createApp(App)
+let app: Nullable<IApp> = null
+let router: Nullable<Router> = null
+let history: Nullable<RouterHistory> = null
 
-app.use(router)
+window.mount = () => {
+  app = createApp(App)
+  history = createWebHashHistory('')
+  router = createRouter({
+    history,
+    routes
+  })
 
-useElementPlus(app)
-useErrorHandler(app)
+  useElementPlus(app)
+  useErrorHandler(app)
 
-app.mount('#app')
+  app.use(router)
+  app.mount('#app')
+}
 
 window.unmount = () => {
-  app.unmount()
+  app?.unmount()
+  history?.destroy()
+
+  app = null
+  router = null
+  history = null
+}
+
+if (!window.__MICRO_APP_ENVIRONMENT__) {
+  window.mount()
 }
